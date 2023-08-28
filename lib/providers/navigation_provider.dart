ccc991090a575491221052a42b0a2c390532fa2d
leeyiwan <leeyiwan2001@gmail.com>
2023-08-28 14:58:52 +0800

    add functions to navigate

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -9,4 +9,29 @@ class NavigationProvider extends ChangeNotifier {
     _currentIndex = index;
     notifyListeners();
   }
+
+  void goToHome() {
+    _currentIndex = 0;
+    notifyListeners();
+  }
+
+  void goToTracker() {
+    _currentIndex = 1;
+    notifyListeners();
+  }
+
+  void goToAnalytics() {
+    _currentIndex = 2;
+    notifyListeners();
+  }
+
+  void goToSplitMoney() {
+    _currentIndex = 3;
+    notifyListeners();
+  }
+
+  void goToGoal() {
+    _currentIndex = 4;
+    notifyListeners();
+  }
 }
