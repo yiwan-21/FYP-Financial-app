af8cdca99dfd37d661a9ff0abd029ab0019af443
leeyiwan <leeyiwan2001@gmail.com>
2023-08-30 17:18:36 +0800

    fix navigation goTo functions

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,37 +1,43 @@
+import 'package:financial_app/pages/navigation.dart';
 import 'package:flutter/material.dart';
 
 class NavigationProvider extends ChangeNotifier {
   int _currentIndex = 2; // starting page
 
-  int get getCurrentIndex => _currentIndex;
+  int get currentIndex => _currentIndex;
 
   void setCurrentIndex(int index) {
     _currentIndex = index;
     notifyListeners();
   }
 
-  void goToHome() {
-    _currentIndex = 0;
-    notifyListeners();
-  }
-
   void goToTracker() {
-    _currentIndex = 1;
+    _currentIndex = 0;
+    Navigation.appBarKey.currentState!.animateTo(0);
     notifyListeners();
   }
 
   void goToAnalytics() {
+    _currentIndex = 1;
+    Navigation.appBarKey.currentState!.animateTo(1);
+    notifyListeners();
+  }
+
+  void goToHome() {
     _currentIndex = 2;
+    Navigation.appBarKey.currentState!.animateTo(2);
     notifyListeners();
   }
 
   void goToSplitMoney() {
     _currentIndex = 3;
+    Navigation.appBarKey.currentState!.animateTo(3);
     notifyListeners();
   }
 
   void goToGoal() {
     _currentIndex = 4;
+    Navigation.appBarKey.currentState!.animateTo(4);
     notifyListeners();
   }
 }
