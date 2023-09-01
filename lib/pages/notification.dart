35f4f5be764e41e9251b842fa915a4e43f38be82
leeyiwan <leeyiwan2001@gmail.com>
2023-09-01 16:09:25 +0800

    notification menu - streambuilder

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -1,8 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../models/notifications.dart';
-import '../providers/notification_provider.dart';
+import '../services/notification_service.dart';
 
 class NotificationMenu extends StatefulWidget {
   const NotificationMenu({super.key});
@@ -12,80 +12,94 @@ class NotificationMenu extends StatefulWidget {
 }
 
 class _NotificationMenuState extends State<NotificationMenu> {
-  void _showNotificationMenu(BuildContext context) async {
-    final selected = await showMenu<NotificationModel>(
-      context: context,
-      position: RelativeRect.fromLTRB(
-        MediaQuery.of(context).size.width, // Right aligned
-        kToolbarHeight * 1.2, // Top aligned
-        0,
-        0,
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      elevation: 5,
-      constraints: BoxConstraints(
-        minWidth: MediaQuery.of(context).size.width * 0.7,
-        maxWidth: MediaQuery.of(context).size.width * 0.7,
-        maxHeight: MediaQuery.of(context).size.height * 0.8,
-      ),
-      items: [
-        const PopupMenuItem<NotificationModel>(
-          enabled: false, // Disable selecting the main title
-          child: ListTile(
-            title: Text(
-              'Notifications',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        ...Provider.of<NotificationProvider>(context, listen: false).notifications.reversed.map((notification) {
-          return PopupMenuItem<NotificationModel>(
-            value: notification,
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 5),
-              color: notification.read ? Colors.white : Colors.grey[100],
-              child: ListTile(
-                title: Text(notification.title),
-                subtitle: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        notification.message,
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Text(
-                      notification.time.toString().substring(11, 16),
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        }).toList(),
-      ],
-    );
-
-    if (selected != null) {
-      // Handle the selected notification
-      debugPrint('Selected: ${selected.title}');
-      setState(() {
-        selected.read = true;
-      });
-      selected.navigateTo();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.notifications),
-      onPressed: () {
-        _showNotificationMenu(context);
+    return StreamBuilder<QuerySnapshot>(
+      stream: NotificationService.getNotificationStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              // Handle loading state
+            },
+          );
+        } else if (snapshot.hasError) {
+          return IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              // Handle error state
+            },
+          );
+        } else {
+          final notifications = snapshot.data == null ? [] : snapshot.data!.docs;
+          debugPrint('Notifications: ${notifications.length}');
+          return PopupMenuButton<NotificationModel>(
+            position: PopupMenuPosition.under,
+            offset: const Offset(30, 0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            elevation: 5,
+            icon: const Icon(Icons.notifications),
+            onSelected: (selected) {
+              debugPrint('Selected: ${selected.title}');
+              setState(() {
+                selected.read = true;
+              });
+              selected.navigateTo();
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<NotificationModel>(
+                  enabled: false,
+                  child: ListTile(
+                    title: Text(
+                      'Notifications',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                
+                ...notifications.map((doc) {
+                  final notification = NotificationService.getNotificationModel(doc['type'], id: doc['functionID']);
+                  if (notification == null) {
+                    return PopupMenuItem<NotificationModel>(
+                      value: null,
+                      child: Container(),
+                    );
+                  }
+                  return PopupMenuItem<NotificationModel>(
+                    value: notification,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 5),
+                      color: notification.read ? Colors.white : Colors.grey[100],
+                      child: ListTile(
+                        title: Text(notification.title),
+                        subtitle: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                notification.message,
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Text(
+                              notification.time.toString().substring(11, 16),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ];
+            },
+          );
+        }
       },
     );
   }
 }
+
