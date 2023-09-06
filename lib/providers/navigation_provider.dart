dbd8dd4c68e32f0fcbdd7c4f6d4d27667c444e04
JoceFoo <jlyjoce@gmail.com>
2023-09-06 17:57:48 +0800

    add more in nav bar
    Co-authored-by: Wan <leeyiwan2001@gmail.com>

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -2,47 +2,83 @@ import 'package:financial_app/pages/navigation.dart';
 import 'package:flutter/material.dart';
 
 class NavigationProvider extends ChangeNotifier {
-  int _currentIndex = 2; // starting page
+  int _navIndex = 2; // starting page
+  int _pageIndex = 2; // starting page
+  bool _isMoreTabActive = false;
 
-  int get currentIndex => _currentIndex;
+  int get navIndex => _navIndex;
+  int get pageIndex => _pageIndex;
+  bool get isMoreTabActive => _isMoreTabActive;
 
-  void setCurrentIndex(int index) {
-    _currentIndex = index;
+  void setIndex(int navIndex, int pageIndex) {
+    _navIndex = navIndex;
+    _pageIndex = pageIndex;
+    closeMoreTab();
+    notifyListeners();
+  }
+
+  void toggleMoreTab() {
+    _isMoreTabActive = !_isMoreTabActive;
+    notifyListeners();
+  }
+
+  void closeMoreTab() {
+    _isMoreTabActive = false;
     notifyListeners();
   }
 
   void reset() {
-    _currentIndex = 2;
-    notifyListeners();
-  }
-
-  void goToTracker() {
-    _currentIndex = 0;
-    Navigation.appBarKey.currentState!.animateTo(0);
-    notifyListeners();
-  }
-
-  void goToAnalytics() {
-    _currentIndex = 1;
-    Navigation.appBarKey.currentState!.animateTo(1);
-    notifyListeners();
-  }
-
-  void goToHome() {
-    _currentIndex = 2;
-    Navigation.appBarKey.currentState!.animateTo(2);
+    _navIndex = 2;
+    _pageIndex = 2;
+    closeMoreTab();
     notifyListeners();
   }
 
   void goToSplitMoney() {
-    _currentIndex = 3;
-    Navigation.appBarKey.currentState!.animateTo(3);
+    _navIndex = 0;
+    _pageIndex = 0;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
     notifyListeners();
   }
 
   void goToGoal() {
-    _currentIndex = 4;
-    Navigation.appBarKey.currentState!.animateTo(4);
+    _navIndex = 1;
+    _pageIndex = 1;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
+    notifyListeners();
+  }
+
+  void goToHome() {
+    _navIndex = 2;
+    _pageIndex = 2;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
+    notifyListeners();
+  }
+
+  void goToTracker() {
+    _navIndex = 3;
+    _pageIndex = 3;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
+    notifyListeners();
+  }
+
+  void goToAnalytics() {
+    _navIndex = 4;
+    _pageIndex = 4;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
+    notifyListeners();
+  }
+
+  void goToBudgeting() {
+    _navIndex = 4;
+    _pageIndex = 5;
+    Navigation.appBarKey.currentState!.animateTo(_navIndex);
+    closeMoreTab();
     notifyListeners();
   }
 }
