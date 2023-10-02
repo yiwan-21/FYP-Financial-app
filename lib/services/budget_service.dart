0db87722dde722b99dca096f6bd948432b43fbda
JoceFoo <jlyjoce@gmail.com>
2023-10-02 14:52:52 +0800

    feat-budget reset&startdate reload

--- lib/services/budget_service.dart
+++ lib/services/budget_service.dart
@@ -204,7 +204,7 @@ class BudgetService {
   }
 
   static Future<void> resetBudget() async {
-    if (FirebaseInstance.auth.currentUser == null || _uid == '') {
+    if (FirebaseInstance.auth.currentUser == null || _uid == '' || getOnlyDate(startingDate) == getOnlyDate(DateTime.now())) {
       await setDocumentID();
     }
 
