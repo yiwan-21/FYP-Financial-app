c7372efde4a7d2f1be779de997ae225bca689399
leeyiwan <leeyiwan2001@gmail.com>
2023-10-04 20:00:07 +0800

    feat split group request
    Co-authored-by: Jocelyn Foo Xiao Yun <JoceFoo@users.noreply.github.com>

--- lib/pages/split_money.dart
+++ lib/pages/split_money.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../components/split_group_request.dart';
 import '../constants/constant.dart';
 import '../constants/style_constant.dart';
 import '../components/manage_group.dart';
@@ -17,6 +18,10 @@ class SplitMoney extends StatefulWidget {
 class _SplitMoneyState extends State<SplitMoney> {
   final Stream<QuerySnapshot> _stream = SplitMoneyService.getGroupStream();
 
+  bool get _topDownAlign {
+    return Constant.isMobile(context) || Constant.isTablet(context);
+  }
+
   void addGroup() {
     showDialog(
       context: context,
@@ -29,44 +34,86 @@ class _SplitMoneyState extends State<SplitMoney> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Align(
+        alignment: _topDownAlign ? Alignment.bottomCenter : Alignment.topCenter,
         child: ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxWidth: 768,
-          ),
-          child: StreamBuilder<QuerySnapshot>(
-            stream: _stream,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (snapshot.hasError) {
-                return Text('Something went wrong: ${snapshot.error}');
-              }
-              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return const Center(
-                  child: Text('No group yet'),
-                );
-              }
+          constraints: const BoxConstraints(maxWidth: 768 * (3 / 2)),
+          child: Flex(
+            direction: _topDownAlign ? Axis.vertical : Axis.horizontal,
+            verticalDirection: VerticalDirection.up,
+            mainAxisAlignment:
+                _topDownAlign ? MainAxisAlignment.end : MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    if (!_topDownAlign)
+                      Container(
+                        alignment: Alignment.topLeft,
+                        margin: const EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(100, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                          ),
+                          onPressed: addGroup,
+                          child: const Text('Add Group'),
+                        ),
+                      ),
+                    Flexible(
+                      child: StreamBuilder<QuerySnapshot>(
+                        stream: _stream,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          if (snapshot.hasError) {
+                            return Text(
+                                'Something went wrong: ${snapshot.error}');
+                          }
+                          if (!snapshot.hasData ||
+                              snapshot.data!.docs.isEmpty) {
+                            return const Center(
+                              child: Text('No group yet'),
+                            );
+                          }
 
-              List<SplitGroupCard> groupCards = snapshot.data!.docs
-                  .map((doc) => SplitGroupCard(doc.id, groupName: doc['name']))
-                  .toList();
-              return ListView(
-                children: List.generate(
-                  groupCards.length,
-                  (index) {
-                    return groupCards[index];
-                  },
+                          List<SplitGroupCard> groupCards = snapshot.data!.docs
+                              .map((doc) => SplitGroupCard(doc.id,
+                                  groupName: doc['name']))
+                              .toList();
+                          return ListView(
+                            shrinkWrap: true,
+                            physics: const BouncingScrollPhysics(),
+                            children: List.generate(
+                              groupCards.length,
+                              (index) {
+                                return groupCards[index];
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
+              ),
+              const Divider(height: 2),
+              Flexible(
+                flex: _topDownAlign ? 0 : 1,
+                child: const GroupRequest(),
+              ),
+            ],
           ),
         ),
       ),
-      floatingActionButton: Constant.isMobile(context)
+      floatingActionButton: _topDownAlign
           ? FloatingActionButton(
               backgroundColor: ColorConstant.lightBlue,
               onPressed: addGroup,
@@ -76,22 +123,75 @@ class _SplitMoneyState extends State<SplitMoney> {
                 color: Colors.black,
               ),
             )
-          : Stack(
-              children: [
-                Positioned(
-                    right: (MediaQuery.of(context).size.width - 768) / 2,
-                    bottom: 5,
-                    child: FloatingActionButton(
-                      backgroundColor: ColorConstant.lightBlue,
-                      onPressed: addGroup,
-                      child: const Icon(
-                        Icons.group_add_outlined,
-                        size: 27,
-                        color: Colors.black,
-                      ),
-                    ))
-              ],
+          : null,
+    );
+  }
+}
+
+class GroupRequest extends StatefulWidget {
+  const GroupRequest({super.key});
+
+  @override
+  State<GroupRequest> createState() => _GroupRequestState();
+}
+
+class _GroupRequestState extends State<GroupRequest> {
+  bool _expanded = true;
+
+  bool get _topDownAlign {
+    return Constant.isMobile(context) || Constant.isTablet(context);
+  }
+
+  void _toggleExpanded() {
+    setState(() {
+      _expanded = !_expanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+      height: _expanded
+          ? (_topDownAlign ? 200 : MediaQuery.of(context).size.height)
+          : 50,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            visualDensity: VisualDensity.compact,
+            onTap: _toggleExpanded,
+            enabled: _topDownAlign,
+            horizontalTitleGap: 0,
+            title: Text(
+              'Group Requests', 
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: _topDownAlign ? FontWeight.normal : FontWeight.bold,
+                fontSize: _topDownAlign ? 16 : 18,
+              ),
             ),
+            leading: _topDownAlign
+                ? _expanded
+                    ? const Icon(Icons.keyboard_arrow_down)
+                    : const Icon(Icons.keyboard_arrow_right)
+                : const Icon(Icons.mail),
+          ),
+          if (_expanded)
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return SplitGroupRequest(
+                      'Testing 123 123 123 123 Group $index', 'inviter 1');
+                },
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
