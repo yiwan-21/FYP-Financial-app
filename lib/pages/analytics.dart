58a77691c6c76e2f4f74d9f6e70fee09cef977ba
JoceFoo <jlyjoce@gmail.com>
2023-11-11 22:01:17 +0800

    refactor display layout

--- lib/pages/analytics.dart
+++ lib/pages/analytics.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../constants/constant.dart';
 import '../components/expense_income_graph.dart';
 import '../components/auto_dis_chart.dart';
 import '../components/monitor_debt_chart.dart';
@@ -11,35 +12,77 @@ class Analytics extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          const SizedBox(height: 24),
-          const ExpenseIncomeGraph(),
-          const SizedBox(height: 24),
-          const AutoDisChart(),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 768),
-            child: const Center(
-              child: Card(
-                elevation: 5,
-                color: Colors.white,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Autonommous Expenditure: Food, Transportation, Rental, Bill\nDiscretionary Expenditure: Education, Personal Items, Other Expenses',
-                  ),
-                ),
-              ),
+    bool isMobile = Constant.isMobile(context);
+    const double verticalSpacing = 24.0;
+
+    Widget buildCard() {
+      return const Center(
+        child: Card(
+          elevation: 5,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Autonomous Expenditure: Food, Transportation, Rental, Bill\nDiscretionary Expenditure: Education, Personal Items, Other Expenses',
             ),
           ),
-          const SizedBox(height: 24),
-          const MonitorDebtChart(),
-          const SizedBox(height: 24),
-          const MonitorGoalChart(),
-          const SizedBox(height: 24),
-          const DailySurplusChart(),
+        ),
+      );
+    }
+
+    Widget landscapeLayout() {
+      return Column(
+        children: [
+          Row(
+            children: [
+              const Expanded(child: ExpenseIncomeGraph()),
+              Expanded(
+                child: Column(
+                  children: [
+                    const AutoDisChart(),
+                    buildCard(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Expanded(child: MonitorGoalChart()),
+              Expanded(child: MonitorDebtChart()),
+            ],
+          ),
         ],
+      );
+    }
+
+    Widget portraitLayout() {
+      return Column(
+        children: [
+          const ExpenseIncomeGraph(),
+          const SizedBox(height: verticalSpacing),
+          const AutoDisChart(),
+          buildCard(),
+          const SizedBox(height: verticalSpacing),
+          const MonitorGoalChart(),
+          const SizedBox(height: verticalSpacing),
+          const MonitorDebtChart(),
+        ],
+      );
+    }
+
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return ListView(
+            children: [
+              const SizedBox(height: verticalSpacing),
+              isMobile ? portraitLayout() : landscapeLayout(),
+              const SizedBox(height: verticalSpacing),
+              const DailySurplusChart(),
+            ],
+          );
+        },
       ),
     );
   }
