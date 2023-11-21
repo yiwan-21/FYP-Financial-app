b745b1f65c3637d7b6b9db45ee195b8f0855e26d
leeyiwan <leeyiwan2001@gmail.com>
2023-11-21 08:48:02 +0800

    hide fcmToken (not fixed)

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,2 +1,14 @@
 importScripts("https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.15.5/firebase-messaging.js");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYKKuHLL4Hm2V4I3fiFQdcFQC-oTY82Zw",
+  authDomain: "fyp-financial-app.firebaseapp.com",
+  projectId: "fyp-financial-app",
+  storageBucket: "fyp-financial-app.appspot.com",
+  messagingSenderId: "368968416992",
+  appId: "1:368968416992:web:efbd4f20534b8718671a85",
+  measurementId: "G-822XJSCHP3",
+};
+
+firebase.initializeApp({firebaseConfig});
